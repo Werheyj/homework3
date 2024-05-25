751371ff48f31ce9c5f38c8a35fc66c70b4649d7
Tacha <Anastasiykoshe@gmail.com>
2024-05-26 00:07:59 +0300

    Распаковка параметров и параметры функции.

---
+++ hw2.py
@@ -0,0 +1,16 @@
+def print_params(a=1, b='строка', c=True):
+    print(a, b, c)
+
+
+print_params()
+print_params(b= 25)
+print_params(c= [1, 2, 3])
+
+values_list = [2, 'story', False]
+values_dict = {'a': 2, 'b': 'story', 'c': False}
+
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = ['f', 9]
+print_params(*values_list_2, 42)
