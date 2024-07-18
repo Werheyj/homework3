0abc1d73902a2fe7955037aafb3085a73623f471
Tacha <Anastasiykoshe@gmail.com>
2024-07-18 17:48:13 +0300

    Модуль 3 задание 4 Добавила 2 примера

--- hw4.py
+++ hw4.py
@@ -6,4 +6,5 @@ def single_root_words(root_word, *other_words):
     print(same_words)
 
 
-
+single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
+single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
