879e399763857cb6cc43c897c3395be56a7ea162
Tacha <Anastasiykoshe@gmail.com>
2024-07-19 14:09:30 +0300

    Модуль 3 задание 4 Добавила 2 примера Доделала код

--- hw4.py
+++ hw4.py
@@ -1,7 +1,7 @@
 def single_root_words(root_word, *other_words):
     same_words = []
     for word in other_words:
-        if root_word.lower() in word.lower():
+        if root_word.lower() in word.lower() or word.lower() in root_word.lower():
             same_words.append(word)
     print(same_words)
 
